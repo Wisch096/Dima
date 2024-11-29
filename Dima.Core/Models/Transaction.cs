ae7e456c1dda0c5955137fc4bc9547008f8d1cf9
Matheus Wisch <wisch.pg@outlook.com>
2024-11-29 09:02:30 -0300

    :sparkles: Feat: Creation of the transaction model

---
+++ Dima.Core/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿namespace Dima.Core.Models;
+
+public class Transaction
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime PaidOrReceivedAt { get; set; }
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+    public string Type1 { get; set; }
+}
