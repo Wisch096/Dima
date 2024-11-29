970e448bcfb9b94c1b2df17aa9254b8e996b531f
Matheus Wisch <wisch.pg@outlook.com>
2024-11-29 11:30:52 -0300

    :recycle: Refactor: Chaging the transaction type from int to enum

--- Dima.Core/Models/Transaction.cs
+++ Dima.Core/Models/Transaction.cs
@@ -1,4 +1,6 @@
-﻿namespace Dima.Core.Models;
+﻿using Dima.Core.Enums;
+
+namespace Dima.Core.Models;
 
 public class Transaction
 {
@@ -6,9 +8,9 @@ public class Transaction
     public string Title { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
     public DateTime PaidOrReceivedAt { get; set; }
-    public int Type { get; set; }
+    public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
     public decimal Amount { get; set; }
     public long CategoryId { get; set; }
     public Category Category { get; set; } = null!;
-    public string Type1 { get; set; }
+    public string UserId { get; set; } = string.Empty;
 }
