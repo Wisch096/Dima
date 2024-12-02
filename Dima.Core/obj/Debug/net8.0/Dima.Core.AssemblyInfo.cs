b9d0d8eb19ac702d7d86dc80c00cda6b59678953
Matheus Wisch <wisch.pg@outlook.com>
2024-12-02 15:14:45 -0300

    ✨ Feat: Creating migrations and transforming the connection string into user-secrets

--- Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e092a739c51caf2932d3fc74351222a87f797ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e066f999d4ff16aec775c381ab9fc3282e25617c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
