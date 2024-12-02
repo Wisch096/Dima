c95bc8afe7a3e5adaa76c9e614b7f5b6a786d730
Matheus Wisch <wisch.pg@outlook.com>
2024-12-02 19:31:57 -0300

    ✨ Feat: Creating default response

---
+++ Dima.Core/Responses/BaseResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace Dima.Core.Responses;
+
+public class BaseResponse
+{
+    private readonly int _code;
+
+    public BaseResponse(string? data, string message, int code)
+    {
+        Data = data;
+        Message = message;
+        _code = code;
+    }
+    
+    public string? Data { get; set; }
+    public string Message { get; set; } = string.Empty;
+
+    public bool IsSuccess => _code is >= 200 and <= 299; 
+}
