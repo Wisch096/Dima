0c104339bc53ff8bb5542dc599b3d21df7e17496
Matheus Wisch <wisch.pg@outlook.com>
2024-12-02 20:20:49 -0300

    ♻️ Refactor: Greatly improving the default paged response

--- Dima.Core/Responses/PagedResponse.cs
+++ Dima.Core/Responses/PagedResponse.cs
@@ -1,9 +1,35 @@
-﻿namespace Dima.Core.Responses;
+﻿using System.Text.Json.Serialization;
+
+namespace Dima.Core.Responses;
 
 public class PagedResponse<TData> : BaseResponse<TData> 
 {
+    [JsonConstructor]
+    public PagedResponse(
+        TData? data, 
+        int totalCount,
+        int currentPage, 
+        int pageSize
+        ) : base(data)
+    {
+        Data = data;
+        TotalCount = totalCount;
+        CurrentPage = currentPage;
+        PageSize = pageSize;
+    }
+
+    public PagedResponse(
+        TData? data, 
+        int code = Configuration.DefaultStatusCode, 
+        string? message = null
+        ) : base(data, code, message)
+    {
+        
+    }
+    
     public int CurrentPage { get; set; }
-    public int TotalPages { get; set; }
+    public int TotalPages 
+        => (int)Math.Ceiling((double)TotalCount / PageSize);
     public int PageSize { get; set; } = Configuration.DefaultPageSize;
     public int TotalCount { get; set; }
 }
