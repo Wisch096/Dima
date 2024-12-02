bf4ebcf42d10e2d3fa4d0d847efe27133826d6e8
Matheus Wisch <wisch.pg@outlook.com>
2024-12-02 17:07:48 -0300

    ✨ Feat: Chaging to register a category

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,4 +1,5 @@
 using Dima.Api.Data;
+using Dima.Core.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,13 +20,24 @@ builder.Services.AddSwaggerGen(x =>
 });
 builder.Services.AddTransient<Handler>();
 
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dima API V1");
+        c.RoutePrefix = string.Empty; 
+    });
+}
+
 app.MapPost(
-    "/v1/transactions",
+    "/v1/categories",
     (Request request, Handler handler) => handler.Handle(request))
-    .WithName("Transactions: Create")
-    .WithSummary("Cria uma nova transação")
+    .WithName("Categories: Create")
+    .WithSummary("Cria uma nova categoria")
     .Produces<Response>();
 
 app.Run();
@@ -33,11 +45,7 @@ app.Run();
 public class Request
 {
     public string Title { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public int Type { get; set; }
-    public decimal Amount { get; set; }
-    public long CategoryId { get; set; }
-    public string UserId { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 }
 
 public class Response
@@ -46,14 +54,23 @@ public class Response
     public string Title { get; set; } = string.Empty;
 }
 
-public class Handler
+public class Handler(AppDbContext context)
 {
     public Response Handle(Request request)
     {
+        var category = new Category
+        {
+            Title = request.Title,
+            Description = request.Description,
+        };
+        
+        context.Categories.Add(category);
+        context.SaveChanges();
+        
         return new Response
         {
-            Id = 4,
-            Title = request.Title,
+            Id = category.Id,
+            Title = category.Title,
         };
     } 
 }
