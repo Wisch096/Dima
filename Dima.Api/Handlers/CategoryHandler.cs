0a708730cdd799ad447350f3e1fc4b47b83d288d
Matheus Wisch <wisch.pg@outlook.com>
2024-12-03 09:37:05 -0300

    ✨ Feat: Creating category handler and implementing the create category method

---
+++ Dima.Api/Handlers/CategoryHandler.cs
@@ -0,0 +1,53 @@
+﻿using Dima.Api.Data;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Handlers;
+
+public class CategoryHandler(AppDbContext context) : ICategoryHandler
+{
+    public async Task<BaseResponse<Category>> CreateAsync(CreateCategoryRequest request)
+    {
+        try
+        {
+            var category = new Category
+            {
+                UserId = request.UserId,
+                Title = request.Title,
+                Description = request.Description,
+            };
+        
+            await context.Categories.AddAsync(category);
+            await context.SaveChangesAsync();
+
+            return new BaseResponse<Category>(category);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception("Falha ao criar uma categoria");
+        }
+    }
+
+    public Task<BaseResponse<Category>> UpdateAsync(UpdateCategoryRequests request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BaseResponse<Category>> DeleteAsync(DeleteCategoryRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BaseResponse<Category>> GetByIdAsync(GetCategoryByIdRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Category>> GetAllAsync(GetAllCategoriesRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
