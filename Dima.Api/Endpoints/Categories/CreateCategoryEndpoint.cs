d83c958f5ac083a7c492b59c8bdd5f8143fcaab0
Matheus Wisch <wisch.pg@outlook.com>
2024-12-09 20:36:39 -0300

    :sparkles: Feat: Creating generic endpoint

---
+++ Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -0,0 +1,6 @@
+﻿namespace Dima.Api.Endpoints.Categories;
+
+public class CreateCategoryEndpoint
+{
+    
+}
--- Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f70238597b50aa1d3f2c5ca2f9b2cc31dc8884f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b65cc96a1bffe382b7ab76a1e770e71e23b6026")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
