9847ffa04aab55ca3b7f7ca37a6ea5a262bb5ad0
Matheus Wisch <wisch.pg@outlook.com>
2024-12-13 14:41:06 -0300

    :sparkles: Feat: Creation of transaction handler contract

---
+++ Dima.Core/Handlers/ITransactionHandler.cs
@@ -0,0 +1,14 @@
+﻿using Dima.Core.Models;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+
+namespace Dima.Core.Handlers;
+
+public interface ITransactionHandler
+{
+    Task<BaseResponse<Transaction?>> CreateAsync(CreateTransactionRequest request);
+    Task<BaseResponse<Transaction?>> UpdateAsync(UpdateTransactionRequest request);
+    Task<BaseResponse<Transaction?>> DeleteAsync(DeleteTransactionRequest request);
+    Task<BaseResponse<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request);
+    Task<PagedResponse<List<Transaction?>>> GetByPeriodAsync(GetTransactionByPeriodRequest request);
+}
