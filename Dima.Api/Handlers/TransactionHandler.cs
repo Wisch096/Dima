218d8afd33740871406ba17fcae5d735478d3879
Matheus Wisch <wisch.pg@outlook.com>
2024-12-18 15:10:32 -0300

    Feat: Creating transaction handler

---
+++ Dima.Api/Handlers/TransactionHandler.cs
@@ -0,0 +1,34 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Handlers;
+
+public class TransactionHandler : ITransactionHandler
+{
+    public async Task<BaseResponse<Transaction?>> CreateAsync(CreateTransactionRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BaseResponse<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BaseResponse<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BaseResponse<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PagedResponse<List<Transaction?>>> GetByPeriodAsync(GetTransactionByPeriodRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
