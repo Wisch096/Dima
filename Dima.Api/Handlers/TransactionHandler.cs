0122dfe25e1f23bd879ace7f2be3c8cd1b8b1653
Matheus Wisch <wisch.pg@outlook.com>
2024-12-19 16:13:21 -0300

    ✨ Feat: Implementing create transaction

--- Dima.Api/Handlers/TransactionHandler.cs
+++ Dima.Api/Handlers/TransactionHandler.cs
@@ -1,15 +1,37 @@
-﻿using Dima.Core.Handlers;
+﻿using Dima.Api.Data;
+using Dima.Core.Handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Transactions;
 using Dima.Core.Responses;
 
 namespace Dima.Api.Handlers;
 
-public class TransactionHandler : ITransactionHandler
+public class TransactionHandler(AppDbContext context) : ITransactionHandler
 {
     public async Task<BaseResponse<Transaction?>> CreateAsync(CreateTransactionRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var transaction = new Transaction
+            {
+                UserId = request.UserId,
+                CategoryId = request.CategoryId,
+                CreatedAt = DateTime.Now,
+                Amount = request.Amount,
+                PaidOrReceivedAt = request.PaidOrReceivedAt,
+                Title = request.Title,
+                Type = request.Type,
+            };
+        
+            await context.Transactions.AddAsync(transaction);
+            await context.SaveChangesAsync();
+        
+            return new BaseResponse<Transaction?>(transaction, 201, "Transação criada com sucesso!");
+        }
+        catch
+        {
+            return new BaseResponse<Transaction?>(null, 500, "Não foi possível criar a sua transação!");
+        }
     }
 
     public Task<BaseResponse<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
--- Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a7487a7ae8b4f8b44b6c9b3524a0b307637f35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+218d8afd33740871406ba17fcae5d735478d3879")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
