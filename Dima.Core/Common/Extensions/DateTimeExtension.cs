ebbb900875e4ac9816e44d885fed76db2e730ea0
Matheus Wisch <wisch.pg@outlook.com>
2024-12-31 14:57:54 -0300

    ✨ Feat: Creating extension getLastDay

--- Dima.Core/Common/Extensions/DateTimeExtension.cs
+++ Dima.Core/Common/Extensions/DateTimeExtension.cs
@@ -5,9 +5,11 @@ public static class DateTimeExtension
     public static DateTime GetFirstDay(this DateTime dateTime, int? year = null, int? month = null)
         => new(year ?? dateTime.Year, month ?? dateTime.Month, 1);
 
-    public static DateTime GetLastDay(
-        this DateTime dateTime, int? year = null, int? month = null)
-    {
-        
-    }
+    public static DateTime GetLastDay(this DateTime dateTime, int? year = null, int? month = null)
+        => new DateTime(
+            year ?? dateTime.Year, 
+            month ?? dateTime.Month, 
+            1)
+            .AddMonths(1)
+            .AddDays(-1);
 }
