f31370f01a85662063025f8f570b453182bc42c9
Matheus Wisch <wisch.pg@outlook.com>
2025-01-02 17:29:07 -0300

    ✨ Feat: Creating and implementing getbyid transaction endpoint

---
+++ Dima.Api/Endpoints/Transactions/GetByIdTransactionEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+
+namespace Dima.Api.Endpoints.Transactions;
+
+public class GetByIdTransactionEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapGet("/{id}", HandleAsync)
+            .WithName("Transactions: GetById")
+            .WithSummary("Recupera uma transação")
+            .WithDescription("Recupera uma transação")
+            .WithOrder(4)
+            .Produces<BaseResponse<Transaction?>>();
+
+    private static async Task<IResult> HandleAsync(
+        ITransactionHandler handler, long id)
+    {
+        var request = new GetTransactionByIdRequest
+        {
+            UserId = "matheus@gmail.com",
+            Id = id
+        };
+        
+        var result = await handler.GetByIdAsync(request);
+        return result.IsSuccess 
+            ? TypedResults.Ok(result.Data) 
+            : TypedResults.BadRequest(result.Data);
+    }
+}
