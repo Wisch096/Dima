89c67ccde9baeab50035e96a1a8ac5d60cd4d271
Matheus Wisch <wisch.pg@outlook.com>
2025-01-21 18:38:26 -0300

    dev

--- Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266e96a8221b84ec186902c723ba4d32bd012910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830a46f94111e00a6a033865a93032f33ed37aee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
