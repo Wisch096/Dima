06469874dfcbd68ca8d5942f5929c444ba2b62f7
Matheus Wisch <wisch.pg@outlook.com>
2025-01-22 20:02:18 -0300

    ✨ Feat: Creating configuredevenvironment in app extension

--- Dima.Api/Common/Api/AppExtension.cs
+++ Dima.Api/Common/Api/AppExtension.cs
@@ -1,6 +1,11 @@
 ﻿namespace Dima.Api.Common.Api;
 
-public class AppExtension
+public static class AppExtension
 {
-    
+    public static void ConfigureDevEnvironment(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.MapSwagger().RequireAuthorization();
+    }
 }
