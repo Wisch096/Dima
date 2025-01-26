d7a80ab19e9219cea524f6661dc4d84c7bf646ea
Matheus Wisch <wisch.pg@outlook.com>
2025-01-26 10:18:12 -0300

    :sparkles: Feat: Add new font

--- Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc3b988bdede514d3a84e615a244b75546d0af75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb019b02c0b0ed7c70197780c6db1b87209d6dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
