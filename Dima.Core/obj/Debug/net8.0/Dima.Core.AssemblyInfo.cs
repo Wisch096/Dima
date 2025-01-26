e3e7384a00e178220c9291606b6434dc0e13609d
Matheus Wisch <wisch.pg@outlook.com>
2025-01-26 12:25:03 -0300

    :sparkles: Feat: add system preferences theme (dark or light)

--- Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb019b02c0b0ed7c70197780c6db1b87209d6dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+967cd3c029e78407131413aaaa7e1311b14acb30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
