e2723f0f4b531acec510c7c5998488d047478bb3
Matheus Wisch <wisch.pg@outlook.com>
2025-01-27 19:57:49 -0300

    ✨ Feat: Create concrete class to cookie authentication state

---
+++ Dima.Web/Secutiry/CookieAuthenticationStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Dima.Web.Secutiry;
+
+public class CookieAuthenticationStateProvider(IHttpClientFactory httpClientFactory) : AuthenticationStateProvider, ICookieAuthenticationStateProvider
+{
+    private bool _isAuthenticated = false;
+    private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+    
+    public async Task<bool> CheckAuthenticatedAsync()
+    {
+        await GetAuthenticationStateAsync();
+        return _isAuthenticated;
+    }
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NotifyAuthenticationStateChanged() => base.NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+}
