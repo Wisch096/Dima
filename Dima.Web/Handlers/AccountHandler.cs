c4426f08e09470f94a830c8f6540e7391aaebfbd
Matheus Wisch <wisch.pg@outlook.com>
2025-01-27 17:32:50 -0300

    :sparkles: Feat: add factory pattern and implementing login

---
+++ Dima.Web/Handlers/AccountHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using Dima.Core.Responses;
+
+namespace Dima.Web.Handlers;
+
+public class AccountHandler(IHttpClientFactory httpClientFactory) : IAccountHandler
+{
+    private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+    
+    public async Task<BaseResponse<string>> LoginAsync(LoginRequest request)
+    {
+        await _client.PostAsJsonAsync("v1/identity/login", request);
+    }
+
+    public Task<BaseResponse<string>> RegisterAsync(RegisterRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task LogoutAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
