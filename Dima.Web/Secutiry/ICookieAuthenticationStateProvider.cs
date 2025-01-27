1c89e47bd74541430f8942e70c7db7cd448e1d2d
Matheus Wisch <wisch.pg@outlook.com>
2025-01-27 19:49:40 -0300

    :sparkles: Feat: add authentication state provider

---
+++ Dima.Web/Secutiry/ICookieAuthenticationStateProvider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Dima.Web.Secutiry;
+
+public interface ICookieAuthenticationStateProvider
+{
+    Task<bool> CheckAuthenticatedAsync();
+    Task<AuthenticationState> GetAuthenticationStateAsync();
+    void NotifyAuthenticationStateChanged();
+}
