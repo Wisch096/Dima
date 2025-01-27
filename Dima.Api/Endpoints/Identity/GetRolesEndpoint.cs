e3f2c817e50f57cebcd0d0154d9121bd33f45139
Matheus Wisch <wisch.pg@outlook.com>
2025-01-27 11:37:09 -0300

    :sparkles: Feat: create account for identity

--- Dima.Api/Endpoints/Identity/GetRolesEndpoint.cs
+++ Dima.Api/Endpoints/Identity/GetRolesEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using Dima.Api.Common.Api;
 using Dima.Api.Models;
+using Dima.Core.Models.Account;
 using Microsoft.AspNetCore.Identity;
 
 namespace Dima.Api.Endpoints.Identity;
@@ -21,13 +22,13 @@ public class GetRolesEndpoint : IEndpoint
    
         var roles = identity
             .FindAll(identity.RoleClaimType)
-            .Select(c => new
+            .Select(c => new RoleClaim
             {
-                c.Issuer,
-                c.OriginalIssuer,
-                c.Type,
-                c.Value,
-                c.ValueType
+                Issuer = c.Issuer,
+                OriginalIssuer = c.OriginalIssuer,
+                Type = c.Type,
+                Value = c.Value,
+                ValueType = c.ValueType
             });
         
         return Task.FromResult<IResult>(TypedResults.Json(roles));
