c0613182dec84d75164a08f9e1541cf96054bbfc
Matheus Wisch <wisch.pg@outlook.com>
2025-01-28 15:46:28 -0300

    :sparkles: Feat: add claims

--- Dima.Web/Secutiry/CookieAuthenticationStateProvider.cs
+++ Dima.Web/Secutiry/CookieAuthenticationStateProvider.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using System.Net.Http.Json;
+using System.Security.Claims;
+using Dima.Core.Models.Account;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Dima.Web.Secutiry;
 
@@ -13,10 +16,33 @@ public class CookieAuthenticationStateProvider(IHttpClientFactory httpClientFact
         return _isAuthenticated;
     }
 
+    public void NotifyAuthenticationStateChanged() => base.NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    
     public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
         throw new NotImplementedException();
     }
 
-    public void NotifyAuthenticationStateChanged() => base.NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    private async Task<User?> GetUser()
+    {
+        try
+        {
+            return await _client.GetFromJsonAsync<User>("v1/identity/manage/info");
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    private async Task<List<Claim>> GetClaims(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new(ClaimTypes.Name, user.Email),
+            new(ClaimTypes.Email, user.Email),
+        };
+        
+        return claims;
+    }
 }
