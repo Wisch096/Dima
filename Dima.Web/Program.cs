9d13ada3c5df7e81190aade69e23bcade1c294e9
Matheus Wisch <wisch.pg@outlook.com>
2025-01-28 17:46:20 -0300

    ✨ Feat: Applying code behind

--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -1,22 +1,28 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Dima.Web;
+using Dima.Web.Secutiry;
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
+builder.Services.AddScoped<CookieHandler>();
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+builder.Services.AddScoped(x => x.GetRequiredService<AuthenticationStateProvider>());
 
 builder.Services.AddMudServices();
 
-builder.Services.AddHttpClient(Configuration.HttpClientName, opt =>
-{
-    opt.BaseAddress = new Uri(Configuration.BackendUrl);
-}).AddHttpMessageHandler<CookieHandler>();
+builder.Services
+    .AddHttpClient(Configuration.HttpClientName, opt => { opt.BaseAddress = new Uri(Configuration.BackendUrl); })
+    .AddHttpMessageHandler<CookieHandler>();
 
 await builder.Build().RunAsync();
