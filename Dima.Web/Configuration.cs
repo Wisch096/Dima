33e551c5058acadb97e81f7f5a0d5f17de770732
Matheus Wisch <wisch.pg@outlook.com>
2025-02-03 19:35:30 -0300

    :sparkles: Feat: Create category and transaction handlers for api

--- Dima.Web/Configuration.cs
+++ Dima.Web/Configuration.cs
@@ -1,4 +1,5 @@
 ﻿using MudBlazor;
+using MudBlazor.Utilities;
 
 namespace Dima.Web;
 
@@ -18,15 +19,15 @@ public static class Configuration
         },
         PaletteLight = new PaletteLight
         {
-            Primary = "#1EFA2D",
+            Primary = new MudColor("#1EFA2D"),
+            PrimaryContrastText = new MudColor("#000000"),
             Secondary = Colors.LightGreen.Darken3,
             Background = Colors.Gray.Lighten4,
-            AppbarBackground = "#1EFA2D",
+            AppbarBackground = new MudColor("#1EFA2D"),
             AppbarText = Colors.Shades.Black,
             TextPrimary = Colors.Shades.Black,
-            PrimaryContrastText = Colors.Shades.Black,
-            DrawerText = Colors.Shades.Black,
-            DrawerBackground = Colors.LightGreen.Lighten4
+            DrawerText = Colors.Shades.White,
+            DrawerBackground = Colors.Green.Darken4
         },
         PaletteDark = new PaletteDark
         {
@@ -34,6 +35,7 @@ public static class Configuration
             Secondary = Colors.LightGreen.Darken3,
             AppbarBackground = Colors.LightGreen.Accent3,
             AppbarText = Colors.Shades.Black,
+            PrimaryContrastText = new MudColor("#000000")
         },
     };
 }
---
+++ Dima.Web/Handlers/CategoryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+
+namespace Dima.Web.Handlers;
+
+public class CategoryHandler : ICategoryHandler
+{
+    public Task<BaseResponse<Category>> CreateAsync(CreateCategoryRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BaseResponse<Category?>> UpdateAsync(UpdateCategoryRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BaseResponse<Category?>> DeleteAsync(DeleteCategoryRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BaseResponse<Category?>> GetByIdAsync(GetCategoryByIdRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PagedResponse<List<Category>>> GetAllAsync(GetAllCategoriesRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
