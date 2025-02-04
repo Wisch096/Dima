2cca5ceebe862caa9ff4a609c431c4e2ea08c52d
Matheus Wisch <wisch.pg@outlook.com>
2025-02-04 12:38:26 -0300

    :sparkles: Feat: Implementing transaction handler

--- Dima.Web/Handlers/TransactionHandler.cs
+++ Dima.Web/Handlers/TransactionHandler.cs
@@ -1,33 +1,43 @@
-﻿using Dima.Core.Handlers;
+﻿using System.Net.Http.Json;
+using Dima.Core.Handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Transactions;
 using Dima.Core.Responses;
 
 namespace Dima.Web.Handlers;
 
-public class TransactionHandler : ITransactionHandler
+public class TransactionHandler(IHttpClientFactory httpClientFactory) : ITransactionHandler
 {
-    public Task<BaseResponse<Transaction?>> CreateAsync(CreateTransactionRequest request)
+    private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+    
+    public async Task<BaseResponse<Transaction?>> CreateAsync(CreateTransactionRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.PostAsJsonAsync("v1/transactions", request);
+        return await result.Content.ReadFromJsonAsync<BaseResponse<Transaction?>>()
+            ?? new BaseResponse<Transaction?>(null, 400, "Não foi possível criar sua transação");
     }
 
-    public Task<BaseResponse<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
+    public async Task<BaseResponse<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.PutAsJsonAsync($"v1/transactions/{request.Id}", request);
+        return await result.Content.ReadFromJsonAsync<BaseResponse<Transaction?>>()
+               ?? new BaseResponse<Transaction?>(null, 400, "Não foi possível atualizar sua transação");
     }
 
-    public Task<BaseResponse<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
+    public async Task<BaseResponse<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.DeleteAsync($"v1/transactions/{request.Id}");
+        return await result.Content.ReadFromJsonAsync<BaseResponse<Transaction?>>()
+               ?? new BaseResponse<Transaction?>(null, 400, "Não foi possível excluir sua transação");
     }
 
-    public Task<BaseResponse<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
+    public async Task<BaseResponse<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
     {
-        throw new NotImplementedException();
+        return await _client.GetFromJsonAsync<BaseResponse<Transaction?>>($"v1/transactions/{request.Id}") 
+               ?? new BaseResponse<Transaction?>(null, 400, "Não foi possível obter a transação");
     }
 
-    public Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionByPeriodRequest request)
+    public async Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionByPeriodRequest request)
     {
         throw new NotImplementedException();
     }
